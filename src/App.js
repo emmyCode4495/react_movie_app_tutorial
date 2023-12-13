5bd23c4bd58e133e02c44ed5282934f26557cc4a
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2023-12-13 12:28:41 +0100

    Completed the turotial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import MovieCard from './MovieCard';
 import SearchIcon from './search.svg'
+
 const API_URL =   'http://www.omdbapi.com/?i=tt3896198&apikey=fc010ea1'
 
 
 const App = () =>{
+    const [movies, setmovies] = useState([])
+    const [searchTerm, setSearchTerm]  = useState('')
+
     const searchMovies = async(title) =>{
         const response = await fetch(`${API_URL}&s={title}`);
         const data = await response.json();
     
-        console.log(data.Search);
+        setmovies(data.Search);
     }  
     
     useEffect(() => {
         searchMovies('Spiderman')
-    }, []);
+    }, []); 
 
     return(
         <div className='app'>
             <h1>MovieLand</h1>
-        </div>
+
+            <div className='search'>
+                <input
+                placeholder='Search for movies'
+                value={searchTerm}
+                onChange={(e)=> setSearchTerm(e.target.value)}
+                />
+                <img
+                src={SearchIcon}
+                alt='search'
+                onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {
+                movies?.length > 0
+                ? (
+                    <div className='container'>
+                       {
+                        movies.map((movie) =>(
+                            <MovieCard movie={movie}/>
+                        ))
+                       }
+                    </div>
+                ) :(
+                    <div>
+                        <h2>No movies found</h2>
+                    </div>
+                ) 
+            }
+          
+        </div> 
     )
 }
 
